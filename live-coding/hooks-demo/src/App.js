ef14de88b589ef05aaed8f585abf3f22a8f9863d
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-05-03 09:10:48 +0200

    setup username rendering

--- live-coding/hooks-demo/src/App.js
+++ live-coding/hooks-demo/src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState('');
+
+  const randomPrefix = Math.floor(Math.random() * 900) + 100;
+
+  console.log(username);
+
   return (
     <>
+      <h2>Amazing website</h2>
+      <p>Username: #{randomPrefix}-{username}</p>
+      <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
     </>
   );
 }
