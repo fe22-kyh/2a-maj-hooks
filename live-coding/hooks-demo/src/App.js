a63258ebc529060d9cb8764d498a114b4f95a269
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-05-03 09:40:08 +0200

    added effect dependency

--- live-coding/hooks-demo/src/App.js
+++ live-coding/hooks-demo/src/App.js
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState([]);
+  const [httpQuery, setHttpQuery] = useState('');
 
   const randomPrefix = Math.floor(Math.random() * 900) + 100;
 
-  console.log(username);
+  useEffect(() => {
+    const getResult = async () => {
+      const data = await fetch(`https://swapi.dev/api/people/?search=${query}`).then(resp => resp.json());
+
+      if(data.count > 0) {
+        setResult(data.results)
+      }
+      console.log(data);
+    };
+
+    getResult();
+  }, [httpQuery]);
+  
+  
+  // om inget har hänt under det senaste 2 sekunderna, uppdatera värdet på http query
+
 
   return (
     <>
       <h2>Amazing website</h2>
-      <p>Username: #{randomPrefix}-{username}</p>
-      <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
+      <p>Query: #{randomPrefix}-{query}</p>
+      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+
+      <p>Character name: {(result.length === 0 && "loading...") || result[0].name}</p>
     </>
   );
 }
