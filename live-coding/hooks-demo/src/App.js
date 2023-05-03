79c093c457174532b98ace030421f2aa369ba137
Jakob Rolandsson <rolandsson.jakob@gmail.com>
2023-05-03 12:03:38 +0200

    addded custom hook to encapsulate fetch operation

--- live-coding/hooks-demo/src/App.js
+++ live-coding/hooks-demo/src/App.js
@@ -1,48 +1,50 @@
 import { useEffect, useState } from "react";
 import CharacterComponent from "./CharacterComponent";
 
+function useQuerySwapi(path) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(undefined);
+  const [data, setData] = useState(undefined); //samma som null
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      const data = await fetch(`https://swapi.dev/api/people/?search=${path}`).then(resp => resp.json());
+
+      if(data.count > 0) {
+        setData(data.results);
+      }
+
+      setIsLoading(false);
+      setError(false);
+    }
+
+    const resultTimeoutId = setTimeout(fetchData, 2000);
+
+    return () => { // det som returneras från en useEffect är en destory funktion (anropas innan komponenten renderas om)
+      clearTimeout(resultTimeoutId);
+    }
+
+  }, [path]);
+
+  return {isLoading, error, data};
+}
+
 function App() {
   const [query, setQuery] = useState('');
-  const [result, setResult] = useState([]);
-  const [httpQuery, setHttpQuery] = useState('');
+  const {isLoading, error, data} = useQuerySwapi(query);
+  // const swapiHook = useQuerySwapi(query);
+  // const isLoading = swapiHook.isLoading;
+  // const error = swapiHook.error;
+  // const data = swapiHook.data;
 
   const randomPrefix = Math.floor(Math.random() * 900) + 100;
 
-  useEffect(() => {
-    const getResult = async () => {
-      const data = await fetch(`https://swapi.dev/api/people/?search=${httpQuery}`).then(resp => resp.json());
-
-      if(data.count > 0) {
-        setResult(data.results)
-      }
-    };
-
-    getResult();
-  }, [httpQuery]);
-  
-  
-  // om inget har hänt under det senaste 2 sekunderna, uppdatera värdet på http query
-  useEffect(() => {
-    setResult([]);
-    const queryTimeoutId = setTimeout(() => {
-      setHttpQuery(query);
-    }, 2000);
-
-    return () => { // det som returneras från en useEffect är en destory funktion (anropas innan komponenten renderas om)
-      clearTimeout(queryTimeoutId); //ta bort timeOut när vi renderas om
-    };
-  }, [query]);
-
-
-  console.log(query);
-
-  let characterComponents = <p>Loading...</p>;
-
-  if(result.length > 0) {
-    characterComponents = result.map(character => 
-      <CharacterComponent data={character} key={crypto.randomUUID()} />
-    );
-  }
+  // data? --> om data är undefined, blir characterComponents undefined
+  let characterComponents = data?.map(character => 
+    // TODO på fredag lägger vi till att expandera character info för särskilda characters, inte alla samtidigt
+    <CharacterComponent data={character} key={crypto.randomUUID()} />
+  );
 
   return (
     <>
@@ -50,7 +52,13 @@ function App() {
       <p>Query: #{randomPrefix}-{query}</p>
       <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
 
-      { characterComponents }
+      { 
+        /*
+          om isLoading=true evaluera <p>Loading...</p>
+          annars evaluera characterComponents
+        */
+        (isLoading && <p>Loading...</p>) || characterComponents
+      }
     </>
   );
 }
